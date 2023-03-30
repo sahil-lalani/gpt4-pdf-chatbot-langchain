d3dd3cd852ad06405e173a92f93c66a0df6cdeb9
Sahil Lalani <sahil.lalani83@gmail.com>
2023-03-29 20:29:46 -0400

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <title>Ask Munger</title>
+      </Head>
       <Head />
       <body>
         <Main />
